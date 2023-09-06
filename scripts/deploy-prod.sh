f7bc67feff3f1f0242c49b88fed29e8bb7199914
Sanil Khurana <sanil@quizizz.com>
2023-09-06 17:17:57 +0530

    fix: Updating script

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ '$BRANCH' != 'main' ]; then
+  echo "Branch is not main, exiting"
+  exit 1
+fi
 luarocks make
 luarocks pack quizizz-scalable-rate-limiter 1.1.0-1
 aws s3 cp quizizz-scalable-rate-limiter-1.1.0-1.all.rock s3://lua-plugins-prod
